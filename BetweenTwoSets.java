d3f876ca56b03b549e1f74f78db268e70fe2052e
Krithicka Sivarajan <krithisowb@gmail.com>
2018-12-26 17:08:45 +0530

    Create BetweenTwoSets.java

---
+++ BetweenTwoSets.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class BetweenTwoSets {
+
+    /*
+     * Complete the getTotalX function below.
+     */
+    static int getTotalX(int[] a, int[] b) {
+        int result=0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int m=a[a.length-1],n=b[0];
+        int q=0,w=0;
+        for(int i=m;i<=n;i++)
+        {
+            q=0;
+            w=0;
+            for(int j=0;j<a.length;j++)
+            {
+            if(i%a[j]==0)
+            q++;
+            }
+            for (int j = 0; j < b.length; j++) {
+                if (b[j] % i == 0)
+                    w++;
+            }
+            if(q==a.length&&w==b.length)
+            result++;
+        }
+return result;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0].trim());
+
+        int m = Integer.parseInt(nm[1].trim());
+
+        int[] a = new int[n];
+
+        String[] aItems = scan.nextLine().split(" ");
+
+        for (int aItr = 0; aItr < n; aItr++) {
+            int aItem = Integer.parseInt(aItems[aItr].trim());
+            a[aItr] = aItem;
+        }
+
+        int[] b = new int[m];
+
+        String[] bItems = scan.nextLine().split(" ");
+
+        for (int bItr = 0; bItr < m; bItr++) {
+            int bItem = Integer.parseInt(bItems[bItr].trim());
+            b[bItr] = bItem;
+        }
+
+        int total = getTotalX(a, b);
+
+        bw.write(String.valueOf(total));
+        bw.newLine();
+
+        bw.close();
+    }
+}
